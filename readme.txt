c3a4e372b0bad2103c4fa4c547a3034d93cf3edb
MARCELO ADRIAN SUAREZ <90062398+marceloasuarez@users.noreply.github.com>
2023-05-25 20:17:02 -0300

    API e Eventos em variaveis

---
+++ readme.txt
@@ -0,0 +1,2 @@
+Rodar o arquivo server.js num servidor NodeJS, e na linha 23 do meu arquivo script.js tem que apuntar ao endereco de esse servidor.
+Eu tenho um servidor NodeJS como exemplo do site da replit.com.
---
+++ server.js
@@ -0,0 +1,11 @@
+var http = require('http');
+http.createServer(function(req, res) { 
+res.setHeader('Access-Control-Allow-Origin', '*');
+res.writeHead(200, { 'Content-Type': 'application/json' });
+let numero = { 
+  temperatura: Math.round(Math.random() * 10),
+  nivel: Math.round(Math.random() * 10),
+  pressao: Math.round(Math.random() * 10)
+}
+res.end(JSON.stringify(numero));
+}).listen(8080);
